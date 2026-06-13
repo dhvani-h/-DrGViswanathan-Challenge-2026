0a24715876f106627091ea1466e3fcb3a7c57e65
dhvani-h <harinkheredhvani@gmail.com>
2026-06-13 22:18:03 +0530

    Update Day22_Solution.cpp

--- Day22_Solution.cpp
+++ Day22_Solution.cpp
@@ -37,3 +37,30 @@ public:
         return ans;
     }
 };
+
+/* 
+Problem: Majority Element
+Platform: LeetCode
+Difficulty: Easy
+Approach: Boyer-Moore Voting Algorithm
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+            if(num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
