a64143a645d088d1b00176d5bf05781bf1020d13
dhvani-h <harinkheredhvani@gmail.com>
2026-06-13 22:28:40 +0530

    Update Day25_Solution.cpp

--- Day25_Solution.cpp
+++ Day25_Solution.cpp
@@ -37,3 +37,22 @@ public:
         return q.empty();
     }
 };
+
+/* 
+Problem: Rotate Array
+Platform: LeetCode
+Difficulty: Medium
+Approach: Array Reversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
