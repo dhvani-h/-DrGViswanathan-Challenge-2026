f1a6e0249acd1002bd6017491c9bf61c17489c5b
dhvani-h <harinkheredhvani@gmail.com>
2026-06-14 21:23:21 +0530

    Create Day27_Solution.cpp

---
+++ Day27_Solution.cpp
@@ -0,0 +1,19 @@
+/* 
+Problem: Path Sum
+Platform: LeetCode
+Difficulty: Easy
+Approach: DFS Recursion
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL) return false;
+        if(root->left == NULL && root->right == NULL) {
+            return targetSum == root->val;
+        }
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
