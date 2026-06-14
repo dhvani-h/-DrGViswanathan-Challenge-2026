cd605ca3af7c692d56cd1c316f8f15479bcc9bf2
dhvani-h <harinkheredhvani@gmail.com>
2026-06-14 21:19:49 +0530

    Create Day26_Solution.cpp

---
+++ Day26_Solution.cpp
@@ -0,0 +1,22 @@
+/* 
+Problem: Binary Tree Inorder Traversal
+Platform: LeetCode
+Difficulty: Easy
+Approach: Recursive DFS (Left → Root → Right)
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+class Solution {
+public:
+    vector<int> ans;
+    void inorder(TreeNode* root) {
+        if(root == NULL) return;
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
+    }
+     vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return ans;
+    }
+};
