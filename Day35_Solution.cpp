1c79f0cb95cabcbac181b75cbde700ad203e181c
dhvani-h <harinkheredhvani@gmail.com>
2026-06-17 19:42:52 +0530

    Update Day35_Solution.cpp

--- Day35_Solution.cpp
+++ Day35_Solution.cpp
@@ -16,3 +16,23 @@ public:
         return left ? left : right;
     }
 };
+
+/*
+Problem: Number of Segments in a String
+Platform: LeetCode
+Difficulty: Easy
+Approach: Count Word Starts
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] != ' ' && (i == 0 || s[i - 1] == ' '))
+                count++;
+        }
+        return count;
+    }
+};
