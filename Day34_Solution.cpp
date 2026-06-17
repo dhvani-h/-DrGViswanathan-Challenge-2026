00c20698308c0630635116d2706db81362f82437
dhvani-h <harinkheredhvani@gmail.com>
2026-06-17 19:40:30 +0530

    Update Day34_Solution.cpp

--- Day34_Solution.cpp
+++ Day34_Solution.cpp
@@ -18,3 +18,25 @@ public:
         prev = root;
     }
 };
+
+/*
+Problem: Length of Last Word
+Platform: LeetCode
+Difficulty: Easy
+Approach: Reverse Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while(i >= 0 && s[i] == ' ') i--;
+        int len = 0;
+        while(i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+        return len;
+    }
+};
