f3c60d090b704736bc46a7bdd0e8c954794b5b1f
dhvani-h <harinkheredhvani@gmail.com>
2026-06-17 19:41:30 +0530

    Create Day35_Solution.cpp

---
+++ Day35_Solution.cpp
@@ -0,0 +1,18 @@
+/*
+Problem: Lowest Common Ancestor of a Binary Tree
+Platform: LeetCode
+Difficulty: Medium
+Approach: Recursive DFS
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left && right) return root;
+        return left ? left : right;
+    }
+};
