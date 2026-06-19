d3207392bd017d57c94fa2f35eae2ee017d78a0f
dhvani-h <harinkheredhvani@gmail.com>
2026-06-19 18:49:09 +0530

    Update Day42_Solution.cpp

--- Day42_Solution.cpp
+++ Day42_Solution.cpp
@@ -18,3 +18,30 @@ public:
         return check(root, LLONG_MIN, LLONG_MAX);
     }
 };
+
+// Problem: Reverse Words in a String
+// LeetCode 151
+// Day 42
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        vector<string> words;
+        string word;
+
+        while(ss >> word)
+            words.push_back(word);
+
+        reverse(words.begin(), words.end());
+
+        string ans = "";
+
+        for(int i = 0; i < words.size(); i++) {
+            if(i > 0) ans += " ";
+            ans += words[i];
+        }
+
+        return ans;
+    }
+};
