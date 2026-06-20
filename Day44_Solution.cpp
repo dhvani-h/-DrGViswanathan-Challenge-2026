590817bd3e84b0a07771b9eab624ea7cffa14e10
dhvani-h <harinkheredhvani@gmail.com>
2026-06-20 21:00:15 +0530

    Update Day44_Solution.cpp

--- Day44_Solution.cpp
+++ Day44_Solution.cpp
@@ -27,3 +27,37 @@ public:
         return build(nums, 0, nums.size() - 1);
     }
 };
+
+// Problem: Maximum Number of Vowels in a Substring of Given Length
+// LeetCode 1456
+// Day 44
+
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+
+    int maxVowels(string s, int k) {
+        int count = 0, ans = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(isVowel(s[i]))
+                count++;
+        }
+
+        ans = count;
+
+        for(int i = k; i < s.size(); i++) {
+            if(isVowel(s[i]))
+                count++;
+
+            if(isVowel(s[i-k]))
+                count--;
+
+            ans = max(ans, count);
+        }
+
+        return ans;
+    }
+};
