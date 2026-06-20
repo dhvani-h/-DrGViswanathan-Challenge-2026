34fe714da76d0f407b15899f75a2ccddcdfcf563
dhvani-h <harinkheredhvani@gmail.com>
2026-06-20 21:02:30 +0530

    Update Day45_Solution.cpp

--- Day45_Solution.cpp
+++ Day45_Solution.cpp
@@ -20,3 +20,19 @@ public:
         return -1;
     }
 };
+
+// Problem: Greatest Common Divisor of Strings
+// LeetCode 1071
+// Day 45
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1)
+            return "";
+
+        int n = gcd(str1.size(), str2.size());
+
+        return str1.substr(0, n);
+    }
+};
