993d0c5d91c9ad0f51fe03e1b6d402f8ea2b225e
dhvani-h <harinkheredhvani@gmail.com>
2026-06-24 21:23:33 +0530

    Create Day49_Solution.cpp

---
+++ Day49_Solution.cpp
@@ -0,0 +1,28 @@
+// Problem: Count Primes
+// LeetCode 204
+// Day 49
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n <= 2) return 0;
+
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
+
+        for(int i = 2; i * i < n; i++) {
+            if(prime[i]) {
+                for(int j = i * i; j < n; j += i)
+                    prime[j] = false;
+            }
+        }
+
+        int count = 0;
+
+        for(bool x : prime) {
+            if(x) count++;
+        }
+
+        return count;
+    }
+};
