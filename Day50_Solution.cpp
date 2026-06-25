7c916ddd562b176ea54b17d9b5067e293b3a8608
dhvani-h <harinkheredhvani@gmail.com>
2026-06-25 22:01:28 +0530

    Create Day50_Solution.cpp

---
+++ Day50_Solution.cpp
@@ -0,0 +1,22 @@
+// Problem: Break a Palindrome
+// LeetCode 1328
+// Day 50
+
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.size();
+
+        if(n == 1) return "";
+
+        for(int i = 0; i < n / 2; i++) {
+            if(palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+
+        palindrome[n - 1] = 'b';
+        return palindrome;
+    }
+};
